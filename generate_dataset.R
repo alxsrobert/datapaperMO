4657c890cbf06d7cfacba748d34ca09fadb536ef
alxsrobert <alexis3.robert3@gmail.com>
2019-08-09 11:33:36 +0100

    turned generate_dataset into a function

--- generate_dataset.R
+++ generate_dataset.R
@@ -1,11 +1,4 @@
-## Generate data for measlesoutbreaker package
-if (Sys.info()[["user"]] %in% c("eidearob", "EIDEAROB")) {
-  setwd("H:/PhD/Study/Measles_clustering_susc/")
-  #Repository with measles data
-  data.dir <- "H:/PhD/Study/Input/"
-  #Repository for output
-  data.out <- "H:/PhD/Study/Output/clusters_USA/figures_comp_epi/no_imports"
-}
+#### Libraries ####
 
 library(magrittr)
 library(dplyr)
@@ -13,9 +6,7 @@ library(expm)
 library(devtools)
 library(Rcpp)
 library(ggplot2)
-# library(measlesoutbreaker)
 library(data.table)
-library(xlsx)
 library(geosphere)
 
 #### Serial interval Measles ####
@@ -47,8 +38,8 @@ f <- dgamma(x = 1:300, scale = 0.4286, shape = 26.834, log = T)
 #### Contact matrices ####
 
 # From Polymod
-polymod <- as.data.table(read.csv(file = "Input/Polymod data/social_contact_UK.csv",
-                                  header = T, colClasses = "numeric"))
+polymod <- as.data.table(read.csv(file = "data/social_contact_UK.csv",
+                                  header = T, , colClasses = "numeric"))
 colnames(polymod) <- substr(x = colnames(polymod), start = 2, stop = nchar(colnames(polymod)))
 #row <- age of contact
 #column <- age of participant
@@ -58,12 +49,12 @@ polymod_prop <- apply(polymod, 2, function(X)
 
 #### Distance Counties pop ####
 
-dt_loca_pop_center <- as.data.table(read.xlsx2(
-  file = paste0(data.dir, "US Data dummy/pop_center.xlsx"),
-  sheetIndex = 1, stringsAsFactors = FALSE, colClasses = c("character", "character",
-                                                           "character","character",
-                                                           "numeric", "numeric",
-                                                           "numeric", "character")))
+dt_loca_pop_center <- as.data.table(read.csv(
+  file = "data/pop_center.csv",
+  stringsAsFactors = FALSE, colClasses = c("character", "character",
+                                           "character","character",
+                                           "numeric", "numeric",
+                                           "numeric", "character")))
 dt_state_county <- dt_loca_pop_center[,.(ID_COUNTY, STNAME)]
 setkey(dt_state_county, ID_COUNTY)
 dt_distance_pop_center <- as.data.table(matrix(0, nrow = nrow(dt_loca_pop_center)**2,
@@ -72,7 +63,8 @@ colnames(dt_distance_pop_center) <- c("county1", "county2", "distance_km",
                                       "pop_county1", "long1", "lat1",
                                       "pop_county2", "long2", "lat2")
 dt_distance_pop_center[, county1 := as.character(county1)]
-dt_distance_pop_center[, county1 := dt_loca_pop_center$ID_COUNTY]
+dt_distance_pop_center[, county1 := rep(dt_loca_pop_center$ID_COUNTY, 
+                                        nrow(dt_loca_pop_center))]
 setkey(dt_loca_pop_center, ID_COUNTY)
 dt_distance_pop_center[, long1 := dt_loca_pop_center[dt_distance_pop_center$county1,
                                                      LONGITUDE]]
@@ -113,10 +105,11 @@ dt_population <- dt_distance[!duplicated(county1), .(county1, pop_county1)]
 colnames(dt_population) <- c("county", "population")
 
 
-#### Generate dataset ####
+#### Function to create new cases ####
 
 new_case <- function(sec, date_index, county_index, age_group_index, genotype_index, 
-                     count, cluster, generation){
+                     count, cluster, generation, dt_distance, 
+                     polymod_prop, w){
   county_k <- sample(dt_distance[county2 == county_index, county1], sec, 
                      prob = dt_distance[county2 == county_index, probs], 
                      replace = T)
@@ -144,89 +137,83 @@ new_case <- function(sec, date_index, county_index, age_group_index, genotype_in
   return(case_k)
 }
 
+#### Generate dataset ####
 
-set.seed(1)
-list_county <- unique(dt_distance$county1)
-pop_county <- dt_distance[!duplicated(county1), pop_county1]
-names(pop_county) <- dt_distance[!duplicated(county1), county1]
-r0_state <- rexp(n = length(unique(dt_state_county$STNAME)), 
-                 rate = 1.5)
-names(r0_state) <- unique(dt_state_county$STNAME)
-r0_state[r0_state>1] <- 0.99
-a <- 0.84
-b <- 0.06
-dt_distance[, nb_commut := pop_county1**a*exp(-distance_km*b)]
-dt_distance[distance_km > 100, nb_commut := 0]
-sum_commut <- dt_distance[, lapply(.SD,sum), by = county2,
-                          .SDcols = "nb_commut"]
-setkey(sum_commut, county2)
-dt_distance[, probs := nb_commut / sum_commut[county2,nb_commut]]
 
-dt_cases <- data.table(NULL)
-nb_cases <- 500
-count <- 1
-i <- 1
-while(dim(dt_cases)[1] < nb_cases){
-  date_i <- sample(x = as.Date("2010-01-01"):as.Date("2016-01-01"), size = 1)
-  date_i <- as.Date(date_i, origin = "1970-01-01")
+generate_dataset <- function(a, b, gamma, dt_distance, polymod_prop, w, nb_cases, 
+         r0_state, pop_county){
   
-  county_i <- sample(x =names(pop_county), size = 1, 
-                   prob = pop_county/sum(pop_county))
+  dt_distance[, nb_commut := pop_county1**a*exp(-distance_km*b)]
+  dt_distance[distance_km > gamma, nb_commut := 0]
+  sum_commut <- dt_distance[, lapply(.SD,sum), by = county2,
+                            .SDcols = "nb_commut"]
+  setkey(sum_commut, county2)
+  dt_distance[, probs := nb_commut / sum_commut[county2,nb_commut]]
   
-  age_group_i <- ceiling(rexp(n = 1, rate = 0.2))
-  if(age_group_i>dim(polymod_prop)[2]) age_group_i <- dim(polymod_prop)[2]
+  dt_cases <- data.table(NULL)
+  count <- 1
+  i <- 1
   
-  genotype_i <- sample(c("Not attributed", "D4", "B1", "B4", "D8"), size = 1, 
-                       prob = c(0.60, 0.20, 0.10, 0.05, 0.05))
-  
-  clust_i <- i
-  gen <- 1
-  case_i <- data.table(as.character(count), dt_state_county[county_i, STNAME],
-                       date_i, genotype_i, county_i, age_group_i, TRUE, clust_i,
-                       gen)
-  colnames(case_i) <- c("ID", "State", "Date", "Genotype", "county", 
-                        "age_group", "import", "cluster", "generation")
-  dt_cases <- rbind(dt_cases, case_i)
-  count <- dim(dt_cases)[1]
-  while(count<= dim(dt_cases)[1]
-        ){
-    date_i <- dt_cases[count, Date]
-    county_i <- dt_cases[count, county]
-    age_group_i <- dt_cases[count, age_group]
-    genotype_i <- dt_cases[count, Genotype]
-    clust_i <- dt_cases[count, cluster]
-    r0_i <- r0_state[dt_state_county[county_i, STNAME]]
-    gen_i <- dt_cases[count, generation]
-    new_cases_i <- rpois(1, r0_i)
-    count <- count + 1
-    if(new_cases_i > 0){
-      case_subs <- new_case(sec = new_cases_i, date_index = date_i, 
-                            county_index = county_i, age_group_index = age_group_i, 
-                            genotype_index = genotype_i, count = count, 
-                            cluster = clust_i, generation = gen_i)
-      dt_cases <- rbind(dt_cases, case_subs)
+  while(dim(dt_cases)[1] < nb_cases){
+    date_i <- sample(x = as.Date("2010-01-01"):as.Date("2016-01-01"), size = 1)
+    date_i <- as.Date(date_i, origin = "1970-01-01")
+    
+    county_i <- sample(x =names(pop_county), size = 1, 
+                     prob = pop_county/sum(pop_county))
+    
+    age_group_i <- ceiling(rexp(n = 1, rate = 0.2))
+    if(age_group_i>dim(polymod_prop)[2]) age_group_i <- dim(polymod_prop)[2]
+    
+    genotype_i <- sample(c("Not attributed", "D4", "B1", "B4", "D8"), size = 1, 
+                         prob = c(0.60, 0.20, 0.10, 0.05, 0.05))
+    
+    clust_i <- i
+    gen <- 1
+    case_i <- data.table(as.character(count), dt_state_county[county_i, STNAME],
+                         date_i, genotype_i, county_i, age_group_i, TRUE, clust_i,
+                         gen)
+    colnames(case_i) <- c("ID", "State", "Date", "Genotype", "county", 
+                          "age_group", "import", "cluster", "generation")
+    dt_cases <- rbind(dt_cases, case_i)
+    count <- dim(dt_cases)[1]
+    while(count<= dim(dt_cases)[1]
+          ){
+      date_i <- dt_cases[count, Date]
+      county_i <- dt_cases[count, county]
+      age_group_i <- dt_cases[count, age_group]
+      genotype_i <- dt_cases[count, Genotype]
+      clust_i <- dt_cases[count, cluster]
+      r0_i <- r0_state[dt_state_county[county_i, STNAME]]
+      gen_i <- dt_cases[count, generation]
+      new_cases_i <- rpois(1, r0_i)
+      count <- count + 1
+      if(new_cases_i > 0){
+        case_subs <- new_case(sec = new_cases_i, date_index = date_i, 
+                              county_index = county_i, age_group_index = age_group_i, 
+                              genotype_index = genotype_i, count = count, 
+                              cluster = clust_i, generation = gen_i, 
+                              dt_distance = dt_distance,
+                              polymod_prop = polymod_prop, w = w)
+        dt_cases <- rbind(dt_cases, case_subs)
+      }
     }
+    i <- i + 1 
   }
-  i <- i + 1 
+  
+  dt_distance <- dt_distance[,.(county1, county2, distance_km)]
+  
+  setkey(dt_distance, county1)
+  distance_matrix <- sapply(unique(dt_distance$county1), 
+                            function(X){
+    return(dt_distance[county2 == X][, distance_km])
+  })
+  rownames(distance_matrix) <- colnames(distance_matrix)
+  
+  vect_pop <- dt_population$population
+  names(vect_pop) <- dt_population$county
+  vect_pop <- vect_pop[rownames(distance_matrix)]
+  
+  fake_outbreak <- list(cases = dt_cases, distance = distance_matrix,
+                        population = vect_pop, age_contact = polymod)
+  return(fake_outbreak)
 }
-
-table(dt_cases[, cluster]) %>% table %>% print
-
-dt_distance <- dt_distance[,.(county1, county2, distance_km)]
-
-setkey(dt_distance, county1)
-distance_matrix <- sapply(unique(dt_distance$county1), 
-                          function(X){
-  return(dt_distance[county2 == X][, distance_km])
-})
-rownames(distance_matrix) <- colnames(distance_matrix)
-
-vect_pop <- dt_population$population
-names(vect_pop) <- dt_population$county
-vect_pop <- vect_pop[rownames(distance_matrix)]
-
-fake_outbreak <- list(cases = dt_cases, distance = distance_matrix,
-                      population = vect_pop, age_contact = polymod)
-
-save(fake_outbreak, 
-     file ="H:/PhD/Study/Measles_clustering_susc/measlesoutbreaker/data/fake_outbreak.RData")
