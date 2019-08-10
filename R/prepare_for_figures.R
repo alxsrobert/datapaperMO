a2188c31ec02978c8a4c2cd3bc66e6a5bc91207c
alxsrobert <alexis3.robert3@gmail.com>
2019-08-10 15:01:49 +0100

    Add function case_state
    case_state is used to calculte the proportion of subsequent cases vs imports in each state

--- R/prepare_for_figures.R
+++ R/prepare_for_figures.R
@@ -1,3 +1,20 @@
+case_state <- function(clust_matrix, dt_cases){
+  clust_matrix_nb <- gsub(pattern = "alpha_", 
+                          replacement = "", 
+                          clust_matrix)
+  colnames(clust_matrix_nb) <- gsub(pattern = "alpha_", replacement = "", 
+                                    colnames(clust_matrix))
+  factor_import <- t(apply(clust_matrix_nb, 1, function(X){
+    import_iter <- which(X == names(X))
+    factor_import <- table(dt_cases[!import_iter, State])/
+      table(dt_cases[import_iter, State])
+    factor_import[!is.finite(factor_import)] <- NA
+    return(factor_import)
+  }))
+  return(factor_import)
+}
+
+
 prepare_for_figures <- function(out, dt_cases, burnin, sample_every, max_clust,
                                 thresh_barplot, diff){
   
@@ -54,10 +71,12 @@ prepare_for_figures <- function(out, dt_cases, burnin, sample_every, max_clust,
   sum_table_tot <- apply(table_tot, 1, sum)
   prop_table_tot <- t(apply(table_tot, 1, function(X) return(X)))
   
-  tot_size_cluster_barplot <- t(rowsum(x = t(prop_table_tot), group = groups_barplot))
+  tot_size_cluster_barplot <- t(rowsum(x = t(prop_table_tot), 
+                                       group = groups_barplot))
   colnames(tot_size_cluster_barplot) <- names_barplot
   
-  size_cluster_singletons <- t(rowsum(x = t(table_singletons), group = groups_barplot))
+  size_cluster_singletons <- t(rowsum(x = t(table_singletons), 
+                                      group = groups_barplot))
   colnames(tot_size_cluster_barplot) <- names_barplot
   
   med_prop_singletons <- apply(size_cluster_singletons, 2, median)
@@ -78,15 +97,12 @@ prepare_for_figures <- function(out, dt_cases, burnin, sample_every, max_clust,
     new <- t(apply(clust_matrix_nb, 1, function(X){
       import_iter <- which(X == names(X))
       index <- length(which(dt_cases[import_iter, import == TRUE]))
-      # import <- length(which(dt_cases[import_iter, import == FALSE] &
-      #                          dt_cases[import_iter, import == 1]))
-      # index <- index + import
       not_import <- length(which(dt_cases[import_iter, import == FALSE] &
                                    dt_cases[import_iter, import == 2]))
       missed <- length(which(!is.element(which(dt_cases$import == 1),
-                                         import_iter)))# / length(which(dt_cases$import == 1))
+                                         import_iter)))
       return(c("Imports" = length(import_iter),
-               "Import status \ncorrectly inferred" = index#,#/length(import_iter),
+               "Import status \ncorrectly inferred" = index
       ))
     }))
     return(new)
@@ -130,8 +146,10 @@ prepare_for_figures <- function(out, dt_cases, burnin, sample_every, max_clust,
     return(misplaced)
   })
   med_inferred_in_clust <- apply(prop_inferred_in_clust, 2, median)
-  up_inferred_in_clust <- apply(prop_inferred_in_clust, 2, function(X) quantile(X, 0.025))
-  low_inferred_in_clust <- apply(prop_inferred_in_clust, 2, function(X) quantile(X, 0.975))
+  up_inferred_in_clust <- apply(prop_inferred_in_clust, 2, 
+                                function(X) quantile(X, 0.025))
+  low_inferred_in_clust <- apply(prop_inferred_in_clust, 2, 
+                                 function(X) quantile(X, 0.975))
   
   dt_prop <- cbind.data.frame(case = not_singletons, 
                               ref = med_clust_in_inferred, 
@@ -158,9 +176,11 @@ prepare_for_figures <- function(out, dt_cases, burnin, sample_every, max_clust,
                           dt_prop$missed<X["missed"]+diff/2)))
   })
   dt_heatmap$prop <- dt_heatmap$number/sum(dt_heatmap$number)
-  y_max <- max(dt_heatmap[prop>0, missed])
-  x_max <- max(dt_heatmap[prop>0, ref])
-  dt_heatmap <- dt_heatmap[missed <= y_max & ref <= x_max,]
+  # y_max <- max(dt_heatmap[prop>0, missed])
+  # x_max <- max(dt_heatmap[prop>0, ref])
+  # dt_heatmap <- dt_heatmap[missed <= y_max & ref <= x_max,]
+
+  factor_import <- case_state(clust_matrix, dt_cases)
   
   return(list(low_size_cluster_barplot = low_size_cluster_barplot,
               med_size_cluster_barplot = med_size_cluster_barplot,
@@ -169,6 +189,7 @@ prepare_for_figures <- function(out, dt_cases, burnin, sample_every, max_clust,
               low = low,
               med = med,
               up = up,
-              dt_heatmap = dt_heatmap))
+              dt_heatmap = dt_heatmap, 
+              factor_import = factor_import))
   
 }
