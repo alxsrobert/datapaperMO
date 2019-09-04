fa61bb735cfc39727f0928a3a6487e7c730857f1
alxsrobert <alexis3.robert3@gmail.com>
2019-09-04 16:41:34 +0100

    Add code to run measlesoutbreaker on different proportion of genotyped cases

--- R/analysis_generated_data.R
+++ R/analysis_generated_data.R
@@ -1,23 +1,29 @@
+## Generate all measlesoutbreaker runs on simulated dataset (toy_outbreak)
+
 source("R/library_importation.R")
-source("R/generate_dataset.R")
+source("R/function_generate_dataset.R")
 source("R/load_data_distributions.R")
 
 # If FALSE: Use data attached to measlesoutbreaker, if TRUE: generate the data
 generate <- TRUE
 
 if(generate){
+  # Set seed
   set.seed(1)
+  # Population per county
   pop_county <- dt_distance[!duplicated(county1), pop_county1]
   names(pop_county) <- dt_distance[!duplicated(county1), county1]
-  
+  # Draw the r0 in each state, following an exponential distribution
   r0_state <- rexp(n = length(unique(dt_state_county$STNAME)), 
                    rate = 1.5)
   names(r0_state) <- unique(dt_state_county$STNAME)
   r0_state[r0_state>1] <- 0.95
-  
+  # Spatial parameters
   a <- 0.84
   b <- 0.08
+  # Spatial threshold
   gamma <- 100
+  # Maximum number of cases in the generated dataset
   nb_cases <- 1000
   
   toy_outbreak <- generate_dataset(a = a, b = b, gamma = gamma, 
@@ -249,3 +255,138 @@ out <- outbreaker(data = data,config = config,priors = priors,
                   likelihoods = likelihoods,moves = moves)
 saveRDS(out, file = paste0("toy_outbreak_runs/import_time_genotype_age.rds"))
 
+
+#### measlesoutbreaker analysis toy data: Different genotype report rate ####
+
+# Set seed
+set.seed(1)
+# Population per county
+pop_county <- dt_distance[!duplicated(county1), pop_county1]
+names(pop_county) <- dt_distance[!duplicated(county1), county1]
+# Draw the r0 in each state, following an exponential distribution
+r0_state <- rexp(n = length(unique(dt_state_county$STNAME)), 
+                 rate = 1.5)
+names(r0_state) <- unique(dt_state_county$STNAME)
+r0_state[r0_state>1] <- 0.95
+# Spatial parameters
+a <- 0.84
+b <- 0.08
+# Spatial threshold
+gamma <- 100
+# Maximum number of cases in the generated dataset
+nb_cases <- 1000
+dt_cases <- toy_outbreak[["cases"]]
+dt_cases <- dt_cases[order(Date), ]
+
+toy_outbreak <- generate_dataset(a = a, b = b, gamma = gamma, 
+                                 dt_distance = dt_distance,
+                                 polymod_prop = polymod_prop, w = w, 
+                                 nb_cases = nb_cases, r0_state = r0_state, 
+                                 pop_county = pop_county, 
+                                 dt_state_county = dt_state_county,
+                                 t_min = as.Date("2010-01-01"), 
+                                 t_max = as.Date("2016-01-01"), prop_gen = 1) 
+
+## No import, threshold = 0.05, all genotype reported
+data <- outbreaker_data(dates = dt_cases$Date,
+                        age_group = dt_cases$age_group,
+                        postcode = dt_cases$county,
+                        w_dens = dnorm(x = 1:300, mean = 11.7, sd = 2.0),
+                        f_dens = dgamma(x = 1:300, scale = 0.4286, shape = 26.834),
+                        a_dens = as.matrix(age_contact),
+                        genotype = dt_cases$Genotype,
+                        is_cluster = NULL,
+                        population = pop_vect,
+                        distance = dist_mat,
+                        import = FALSE)
+
+config <- create_config(data = data, 
+                        init_tree = "star", 
+                        spatial_method = "exponential",
+                        gamma = 100,
+                        delta = 30,
+                        n_iter = 70000, 
+                        sample_every = 50, 
+                        min_date = -10, 
+                        max_kappa = 5,
+                        find_import = TRUE, 
+                        outlier_threshold = 0.05, 
+                        outlier_relative = FALSE,
+                        n_iter_import = 30000, 
+                        sample_every_import = 50, 
+                        burnin = 10000)
+priors <- custom_priors()
+likelihoods <- custom_likelihoods()
+moves <- custom_moves()
+
+out <- outbreaker(data = data,config = config,priors = priors,
+                  likelihoods = likelihoods,moves = moves)
+
+saveRDS(out, file = "toy_outbreak_runs/no_import_thresh005_all_gen.rds")
+
+
+# Set seed
+set.seed(1)
+# Population per county
+pop_county <- dt_distance[!duplicated(county1), pop_county1]
+names(pop_county) <- dt_distance[!duplicated(county1), county1]
+# Draw the r0 in each state, following an exponential distribution
+r0_state <- rexp(n = length(unique(dt_state_county$STNAME)), 
+                 rate = 1.5)
+names(r0_state) <- unique(dt_state_county$STNAME)
+r0_state[r0_state>1] <- 0.95
+# Spatial parameters
+a <- 0.84
+b <- 0.08
+# Spatial threshold
+gamma <- 100
+# Maximum number of cases in the generated dataset
+nb_cases <- 1000
+dt_cases <- toy_outbreak[["cases"]]
+dt_cases <- dt_cases[order(Date), ]
+
+toy_outbreak <- generate_dataset(a = a, b = b, gamma = gamma, 
+                                 dt_distance = dt_distance,
+                                 polymod_prop = polymod_prop, w = w, 
+                                 nb_cases = nb_cases, r0_state = r0_state, 
+                                 pop_county = pop_county, 
+                                 dt_state_county = dt_state_county,
+                                 t_min = as.Date("2010-01-01"), 
+                                 t_max = as.Date("2016-01-01"), prop_gen = 0) 
+
+## No import, threshold = 0.05, no genotype reported
+data <- outbreaker_data(dates = dt_cases$Date,
+                        age_group = dt_cases$age_group,
+                        postcode = dt_cases$county,
+                        w_dens = dnorm(x = 1:300, mean = 11.7, sd = 2.0),
+                        f_dens = dgamma(x = 1:300, scale = 0.4286, shape = 26.834),
+                        a_dens = as.matrix(age_contact),
+                        genotype = dt_cases$Genotype,
+                        is_cluster = NULL,
+                        population = pop_vect,
+                        distance = dist_mat,
+                        import = FALSE)
+
+config <- create_config(data = data, 
+                        init_tree = "star", 
+                        spatial_method = "exponential",
+                        gamma = 100,
+                        delta = 30,
+                        n_iter = 70000, 
+                        sample_every = 50, 
+                        min_date = -10, 
+                        max_kappa = 5,
+                        find_import = TRUE, 
+                        outlier_threshold = 0.05, 
+                        outlier_relative = FALSE,
+                        n_iter_import = 30000, 
+                        sample_every_import = 50, 
+                        burnin = 10000)
+priors <- custom_priors()
+likelihoods <- custom_likelihoods()
+moves <- custom_moves()
+
+out <- outbreaker(data = data,config = config,priors = priors,
+                  likelihoods = likelihoods,moves = moves)
+
+saveRDS(out, file = "toy_outbreak_runs/no_import_thresh005_no_gen.rds")
