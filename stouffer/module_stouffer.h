32196f17e8f827adc702cf91ca3f0fda14483efe
alxsrobert <alexis3.robert3@gmail.com>
2020-01-14 17:26:03 +0000

    Add Stouffer's rank as module

---
+++ stouffer/module_stouffer.h
@@ -0,0 +1,16 @@
+#ifndef STOUFFER_MODULE_STOUFFER_H
+#define STOUFFER_MODULE_STOUFFER_H
+
+#include <Rcpp.h>
+#include <Rmath.h>
+
+// [[Rcpp::export()]]
+Rcpp::List cpp_stouffer_matrix(Rcpp::NumericVector population,
+                               Rcpp::NumericMatrix distance, double a, 
+                               double gamma, int nb_cases);
+// [[Rcpp::export()]]
+Rcpp::List cpp_stouffer_move_a(Rcpp::List param, Rcpp::List data, Rcpp::List config,
+                               Rcpp::RObject custom_ll = R_NilValue,
+                               Rcpp::RObject custom_prior = R_NilValue);
+
+#endif
