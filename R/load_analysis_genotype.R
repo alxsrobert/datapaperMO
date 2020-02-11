b3db81ff1a8abbcbda56c52a941a3a8c7e741e8b
alxsrobert <alexis3.robert3@gmail.com>
2020-02-11 14:56:07 +0000

    toy_outbreak became toy_outbreak_long in o2geosocial

--- R/load_analysis_genotype.R
+++ R/load_analysis_genotype.R
@@ -1,15 +1,15 @@
 #### Load libraries and parameters ####
 
 source("R/library_importation.R")
-data("toy_outbreak")
+data("toy_outbreak_long")
 source("R/function_prepare_for_figures.R")
 source("R/function_generate_figures_main.R")
 source("R/function_supplement_figures.R")
-## Load elements of the list toy_outbreak
-dt_cases <- toy_outbreak[["cases"]]
+## Load elements of the list toy_outbreak_long
+dt_cases <- toy_outbreak_long[["cases"]]
 dt_cases <- dt_cases[order(Date), ]
-dt_cases[, size_cluster := table(cluster)[cluster]]
-dt_regions <- toy_outbreak[["dt_regions"]]
+dt_cases[, size_cluster := table(cluster)[as.character(cluster)]]
+dt_regions <- toy_outbreak_long[["dt_regions"]]
 all_dist <- distGeo(matrix(c(rep(dt_regions$long, nrow(dt_regions)), 
                              rep(dt_regions$lat, nrow(dt_regions))), ncol = 2), 
                     matrix(c(rep(dt_regions$long, each = nrow(dt_regions)), 
@@ -17,14 +17,14 @@ all_dist <- distGeo(matrix(c(rep(dt_regions$long, nrow(dt_regions)),
 
 dist_mat <- matrix(all_dist/1000, nrow = nrow(dt_regions))
 pop_vect <- dt_regions$population
-age_contact <- toy_outbreak[["age_contact"]]
+age_contact <- toy_outbreak_long[["age_contact"]]
 
 burnin <- 10000
 sample_every <- 50
 max_clust <- 500
 
 ## Break points and labels for histogram
-thresh_barplot <- c(1,2,3,5,10,15,20,30,40,50)
+thresh_barplot <- c(1,2,3,5,10, 20,30,50, 100, 200)
 names_barplot <- sapply(1:length(thresh_barplot), function(X){
   if((X + 1) > length(thresh_barplot))
     return(paste0(as.character(thresh_barplot[X]), "+"))
--- R/load_analysis_likelihoods.R
+++ R/load_analysis_likelihoods.R
@@ -1,15 +1,15 @@
 #### Load libraries and parameters ####
 
 source("R/library_importation.R")
-data("toy_outbreak")
+data("toy_outbreak_long")
 source("R/function_prepare_for_figures.R")
 source("R/function_generate_figures_main.R")
 source("R/function_supplement_figures.R")
-## Load elements of the list toy_outbreak
-dt_cases <- toy_outbreak[["cases"]]
+## Load elements of the list toy_outbreak_long
+dt_cases <- toy_outbreak_long[["cases"]]
 dt_cases <- dt_cases[order(Date), ]
-dt_cases[, size_cluster := table(cluster)[cluster]]
-dt_regions <- toy_outbreak[["dt_regions"]]
+dt_cases[, size_cluster := table(cluster)[as.character(cluster)]]
+dt_regions <- toy_outbreak_long[["dt_regions"]]
 all_dist <- distGeo(matrix(c(rep(dt_regions$long, nrow(dt_regions)), 
                              rep(dt_regions$lat, nrow(dt_regions))), ncol = 2), 
                     matrix(c(rep(dt_regions$long, each = nrow(dt_regions)), 
@@ -17,14 +17,14 @@ all_dist <- distGeo(matrix(c(rep(dt_regions$long, nrow(dt_regions)),
 
 dist_mat <- matrix(all_dist/1000, nrow = nrow(dt_regions))
 pop_vect <- dt_regions$population
-age_contact <- toy_outbreak[["age_contact"]]
+age_contact <- toy_outbreak_long[["age_contact"]]
 
 burnin <- 10000
 sample_every <- 50
 max_clust <- 500
 
 ## Break points and labels for histogram
-thresh_barplot <- c(1,2,3,5,10,15,20,30,40,50)
+thresh_barplot <- c(1,2,3,5,10, 20,30,50, 100, 200)
 names_barplot <- sapply(1:length(thresh_barplot), function(X){
   if((X + 1) > length(thresh_barplot))
     return(paste0(as.character(thresh_barplot[X]), "+"))
@@ -74,14 +74,6 @@ fig_ref <- list(med_size_cluster_barplot = size_cluster_ref_barplot,
                 med = med_ref)
 
 #### Load o2geosocial runs ####
-
-## no_like
-out_no_like <- readRDS(file = "toy_outbreak_runs/import_no_like.rds")
-fig_no_like <- prepare_for_figures(out = out_no_like, 
-                                   dt_cases = dt_cases, burnin = burnin, 
-                                   sample_every = sample_every, 
-                                   max_clust = max_clust,
-                                   thresh_barplot = thresh_barplot, diff = diff)
 
 ## time only
 out_time <- readRDS(file = "toy_outbreak_runs/import_time_only.rds")
