9c6d352c65fb5b59ae32ec8d88babda9d9d5a8ab
alxsrobert <alexis3.robert3@gmail.com>
2020-02-13 15:44:15 +0000

    Add legends

--- R/generate_figures.R
+++ R/generate_figures.R
@@ -16,12 +16,22 @@ fig_hist_list <- list(fig_import, fig_ref)
 list_fig_heatmap <- list(fig_import)
 
 generate_figure_4_5(fig_hist_list, list_fig_heatmap)
+par(fig = c(0.5, 1, 0.5, 1))
+legend("topright", fill = c(grey.colors(length(fig_hist_list)), NA), cex = 1.2,
+       border = NA,  pch = c(NA, NA, "_"), pt.cex = 3,
+       legend = c("Epi import", "Epidemiological cluster",
+                  "Correct inference"), bty = "n")
 
 fig_hist_list <- list(fig_001, fig_005, fig_095, fig_005_wi, fig_ref)
 list_fig_heatmap <- list(fig_005, fig_005_wi)
 
 generate_figure_4_5(fig_hist_list, list_fig_heatmap)
-
+par(fig = c(0.5, 1, 0.5, 1))
+legend("right", fill = c(grey.colors(length(fig_hist_list)), NA), 
+       cex = 1.2, border = NA, pch = c(NA, NA, NA, NA, NA, "_"), pt.cex = 3,
+       legend = c("Absolute threshold 1%", "Absolute Threshold 5%", 
+                  "Relative threshold 95%","Absolute threshold 5% + imports",
+                  "Epidemiological cluster", "Correct inference"), bty = "n")
 ## Subsequent cases / number of imports in each state at every iteration
 list_factor_import <- list(fig_005[["factor_import"]], 
                            fig_005_wi[["factor_import"]])
@@ -37,6 +47,11 @@ fig_hist_list <- list(fig_no_gen_sim, fig_005_sim, fig_all_gen_sim, fig_sim)
 list_fig_heatmap <- list(fig_no_gen_sim, fig_all_gen_sim)
 
 generate_figure_4_5(fig_hist_list, list_fig_heatmap)
+par(fig = c(0.5, 1, 0.5, 1))
+legend("right", fill = c(grey.colors(length(fig_hist_list)), NA), 
+       cex = 1.2, border = NA, pch = c(NA, NA, NA, NA, NA, "_"), pt.cex = 3,
+       legend = c("No case genotyped", "60% cases genotyped","All cases genotyped",
+                  "Epidemiological cluster", "Correct inference"), bty = "n")
 
 ##Section 1: description data
 supp_desc_data(dt_cases = dt_cases)
@@ -45,7 +60,9 @@ supp_desc_data(dt_cases = dt_cases)
 ## Precision and sensitivity clustering of cases
 list_fig <- list(fig_import, fig_005, fig_005_wi, 
                  fig_09, fig_095)
-supp_fig_eval_cluster(list_fig, titles = NULL)
+titles <- c("With import", "Absolute 0.05", 
+            "Absolute 0.05 with import", "Relative 90%", "Relative 95%")
+supp_fig_eval_cluster(list_fig, titles = titles)
 
 ## Posterior traces
 list_out <- list(out_import, out_005, out_005_wi, 
@@ -61,28 +78,45 @@ supp_fig_stratified_state(out_095, dt_cases, burnin, sample_every, max_clust)
 
 supp_fig_stratified_state(out_005_wi, dt_cases, burnin, sample_every, max_clust)
 
-## Parameter estimes
+## Parameter estimates
 list_out <- list(out_import, out_005, out_09,
                  out_095, out_005_wi, out_095_wi)
 
 supp_fig_param_estimate(list_out, burnin, sample_every)
+mgp.axis(side = 1, at = 1:length(list_out), 
+         labels = c("With import\n", "Absolute 0.05\n", "Relative 90%\n", 
+                    "Relative 95%\n","Absolute 0.05 \nw/ imports",
+                    "Relative 95% \nw/ imports"), 
+         mgp = c(3.5,2.5,0))
 
 ## Distance transmission
 list_out <- list(out_import, out_005, out_005_wi)
 supp_fig_distance_transmission(list_out = list_out, burnin = burnin, 
                                sample_every = sample_every, dt_cases = dt_cases)
+legend("right", fill = grey.colors(length(list_out)), cex = 1.4, border = NA, 
+       legend = c("With import", "Absolute threshold 5%", 
+                  "Absolute threshold 5% + imports"), bty = "n")
 
 ## Sensitivity components of likelihood 
 fig_hist_list <- list(fig_time, fig_time_gen, fig_time_age, fig_time_spa, fig_ref)
 list_fig_heatmap <- list(fig_time_gen, fig_time_spa)
 
 generate_figure_4_5(fig_hist_list, list_fig_heatmap)
+par(fig = c(0.5, 1, 0.5, 1))
+legend("right", fill = c(grey.colors(length(fig_hist_list)), NA), cex = 1.2,
+       border = NA,  pch = c(rep(NA, length(fig_hist_list)), "_"), pt.cex = 3,
+       legend = c("Time only", "Time and genotype", "Time, genotype and age",
+                  "Time, genotype and space", "Epidemiological cluster",
+                  "Correct inference"),
+       bty = "n")
 
 ## Number of secondary transmission:
 # Overall
 list_out <- list(out_import, out_005, out_005_wi)
 supp_fig_sec_overall(list_out, burnin, sample_every)
+legend("right", fill = grey.colors(length(list_out)), cex = 1.4, border = NA, 
+       legend = c("With import", "Absolute threshold 5%", 
+                  "Absolute threshold 5% + imports"), bty = "n")
 # Stratified by state
 list_out <- list(out_import, out_005, out_005_wi)
 supp_fig_sec_map(list_out, dt_cases)
-
